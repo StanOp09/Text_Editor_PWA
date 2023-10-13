fd4c909dda883df4e798458655cf7af18a0049c1
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-10-13 12:31:28 -0600

    add all necessary files

---
+++ client/src/js/database.js
@@ -0,0 +1,69 @@
+// import { openDB } from 'idb';
+
+// const initdb = async () =>
+//   openDB('jate', 1, {
+//     upgrade(db) {
+//       if (db.objectStoreNames.contains('jate')) {
+//         console.log('jate database already exists');
+//         return;
+//       }
+//       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+//       console.log('jate database created');
+//     },
+//   });
+
+// // TODO: Add logic to a method that accepts some content and adds it to the database
+// export const putDb = async (content) => console.error('putDb not implemented');
+
+// // TODO: Add logic for a method that gets all the content from the database
+// export const getDb = async () => console.error('getDb not implemented');
+
+// initdb();
+
+// ***************
+
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Function to add content to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Add the content to the database
+  const id = await store.add(content);
+
+  await tx.done;
+
+  console.log(`Content with ID ${id} added to the database`);
+};
+
+// Function to get all content from the database
+export const getDb = async () => {
+  const db = await initdb();
+
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  // Retrieve all content from the database
+  const contentArray = await store.getAll();
+
+  await tx.done;
+
+  return contentArray;
+};
+
+initdb();
---
+++ client/src/js/install.js
@@ -0,0 +1,55 @@
+// const butInstall = document.getElementById('buttonInstall');
+
+// // Logic for installing the PWA
+// // TODO: Add an event handler to the `beforeinstallprompt` event
+// window.addEventListener('beforeinstallprompt', (event) => {});
+
+// // TODO: Implement a click event handler on the `butInstall` element
+// butInstall.addEventListener('click', async () => {});
+
+// // TODO: Add an handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {});
+
+// ********
+
+const butInstall = document.getElementById('buttonInstall');
+
+let deferredPrompt; // Store the deferred prompt for later use
+
+// Logic for installing the PWA
+
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default browser prompt
+  event.preventDefault();
+  // Store the event for later use
+  deferredPrompt = event;
+  // Show the install button or perform other actions to notify the user
+  butInstall.style.display = 'block';
+});
+
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    // Show the browser's install prompt
+    deferredPrompt.prompt();
+    // Wait for the user's choice
+    const choiceResult = await deferredPrompt.userChoice;
+    // Reset the deferred prompt variable
+    deferredPrompt = null;
+    // Hide the install button
+    butInstall.style.display = 'none';
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the PWA installation');
+    } else {
+      console.log('User declined the PWA installation');
+    }
+  }
+});
+
+// Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA was installed on the device');
+  // You can perform additional actions if needed
+});
